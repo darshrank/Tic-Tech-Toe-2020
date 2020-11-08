f4ec0893f68145f1c8279834a435c4d2438e41ef
priyansh1211 <68488823+priyansh1211@users.noreply.github.com>
2020-11-08 12:19:26 +0530

    Update login.php

--- login/login.php
+++ login/login.php
@@ -15,12 +15,40 @@
        		<label for="username">Username:</label>
        		<input type="text" name="username" required>
 
-       	</div>
-
+	       </div>
+	       <div>
+       		
+<<<<<<< HEAD
+<<<<<<< HEAD
+       		<label for="roll_number">Roll Number:</label>
+       		<input type="text" name="roll_number" required>
+=======
+       		<label for="email">Email:</label>
+       		<input type="email" name="email" required>
+>>>>>>> 677a7cfc5304d092272624ab688b2a74f37d1e80
+=======
        	<div>
        		
        		<label for="password">Password:</label>
-       		<input type="password" name="password" required>
+       		<input type="password" name="password_1" required>
+       	</div>
+       	<div>
+       		
+       		<label for="password">Confirm Password:</label>
+       		<input type="password" name="password_2" required>
+
+       	</div>
+<<<<<<< HEAD
+<<<<<<< HEAD
+		<div>
+       		
+       		<label for="contact">Parents Contact Number:</label>
+       		<input type="text" name="contact" required>
+       	</div>
+		<div>
+       		
+       		<label for="Address">Parents Address:</label>
+       		<input type="text area" name="addresss" required>
        	</div>
              <button type="submit" name="login"> Log In </button>
              <p>Not a user?<a href="registration.php"><b>Register Here</b></a></p>
